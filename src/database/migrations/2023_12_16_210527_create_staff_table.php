577649c1c3fe06447d4c7b83888e5959976425d1
mcibula <mirkocibula@gmail.com>
2023-12-16 22:09:14 +0100

    Add table and enum for position-holders in staff

---
+++ src/database/migrations/2023_12_16_210527_create_staff_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedSmallInteger('position');
+            $table->string('incumbent_name', 200);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff');
+    }
+};
