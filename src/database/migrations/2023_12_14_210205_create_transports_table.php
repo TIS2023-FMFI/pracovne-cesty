9993d0b96a84127a6a7a214a270aefcdd2d7c2af
mcibula <mirkocibula@gmail.com>
2023-12-17 01:32:43 +0100

    Add visibility column to `transports` table

--- src/database/migrations/2023_12_14_210205_create_transports_table.php
+++ src/database/migrations/2023_12_14_210205_create_transports_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('transports', static function (Blueprint $table) {
             $table->id();
             $table->string('name', 50)->unique();
+            $table->boolean('user_visible')->default(false);
         });
     }
 
