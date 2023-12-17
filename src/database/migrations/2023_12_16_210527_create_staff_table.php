fe7467b59522039980ab55d4a6d2a899ae737a88
mcibula <mirkocibula@gmail.com>
2023-12-17 02:05:09 +0100

    Add position name column into `staff` table

--- src/database/migrations/2023_12_16_210527_create_staff_table.php
+++ src/database/migrations/2023_12_16_210527_create_staff_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->unsignedSmallInteger('position');
+            $table->string('position_name', 100);
             $table->string('incumbent_name', 200);
 
             $table->timestamps();
