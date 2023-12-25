db63d432c63ef65ed87ab12b1b0f1392f70bef34
ihorbaltsat <baltsat1@uniba.sk>
2023-12-25 23:48:03 +0100

    all of the comments changed to PHPDoc

--- src/app/Http/Controllers/BusinessTripController.php
+++ src/app/Http/Controllers/BusinessTripController.php
@@ -6,62 +6,75 @@ use Illuminate\Http\Request;
 
 class BusinessTripController extends Controller
 {
-    
-    //Returning view with details from all trips
+
+    /**
+     * Returning view with details from all trips
+     */
     public function index() {
         return view();
     }
-    
-    //Returning view with the trip details
+
+    /**
+     * Returning view with the trip details
+     */
     public function show(BusinessTrip $b) {
         return view();
     }
-    
-    //Returning view with the form for addding of the new trip
+
+    /**
+     * Returning view with the form for addding of the new trip
+     */
     public function create() {
         return view();
     }
-    
+
     /**
-     * Parsing data from the $request in form 
+     * Parsing data from the $request in form
      * Also managing uploaded files from the form
      * Redirecting to the homepage
      * Sending mail with mail component to admin
      */
     public function store(Request $request) {
-        
+
     }
-    
-    //Returning the view with the trip editing form
+
+    /**
+     * Returning the view with the trip editing form
+     */
     public function edit(BusinessTrip $b) {
         return view();
     }
-    
+
     /**
      * Validation as in create(), should check if user is admin/not
      * Redirecting to the trip editing form
-     * Sending mail with mail component to admin 
+     * Sending mail with mail component to admin
      */
     public function update(Request $request, BusinessTrip $b) {
-        
+
     }
-    
-    //Next 3 functions ccould be combined to changeState(Request $request, BuisenessTrip $b)
-   
-    
-    //Updating state of the trip to cancelled
-    //Adding cancellation reason 
+
+    /**
+     * Next 3 functions could be combined to changeState(Request $request, BuisenessTrip $b)
+     *
+     * Updating state of the trip to cancelled
+     * Adding cancellation reason
+     */
     public function cancel(BusinessTrip $b) {
-        
+
     }
-    
-    //Same as update(), updating state of the trip to confirmed 
+
+    /**
+     * Same as update(), updating state of the trip to confirmed
+     */
     public function confirm() {
-        
+
     }
-    
-    //Same as update, updating state to closed
+
+    /**
+     * Same as update, updating state to closed
+     */
     public function close() {
-        
+
     }
 }
--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -6,29 +6,31 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+
     public function create() {
-        
+
     }
-    
+
     public function store() {
-        
+
     }
-    
+
     public function logout() {
-        
+
     }
-    
+
     public function login() {
-        
+
     }
-    
+
     public function authenticate() {
-        
+
     }
-    
-    //Invitation for external employee
+
+    /**
+     * Invitation for external employee
+     */
     public function invite() {
-        
+
     }
 }
