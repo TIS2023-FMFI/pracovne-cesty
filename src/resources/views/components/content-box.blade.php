2c55034046e79060b3048a7564d56d3d9aeba75b
silviabielikova <XCdSmKj3hXE6Gpn>
2023-12-26 11:16:48 +0100

    added merging of attributes in content box

--- src/resources/views/components/content-box.blade.php
+++ src/resources/views/components/content-box.blade.php
@@ -1,6 +1,6 @@
 @props(['title'])
 
-<div {{ $attributes }} class="d-flex flex-column">
+<div {{ $attributes->merge(['class' => 'd-flex flex-column']) }}>
     <div class="p-3 bg-dark text-uppercase text-white font-weight-bold">{{ $title }}</div>
     <div class="flex-grow-1 bg-white border border-top-0">
         {{ $slot }}
