476190ab5b141222117d9ce4bc7edbfc4c90425d
silviabielikova <XCdSmKj3hXE6Gpn>
2023-12-27 12:02:06 +0100

    added styling to checkbox

--- src/resources/views/components/checkbox.blade.php
+++ src/resources/views/components/checkbox.blade.php
@@ -1,10 +1,14 @@
-<div class="col-md-12">
+<div class="col-md-12 form-check">
     <input
         type="checkbox"
         id="{{ $name }}"
         name="{{ $name }}"
         {{ $isChecked() ? 'checked' : '' }}
         {{ $control != "" ? 'x-model='.$control : '' }}
+        class="form-check-input"
+
     />
-    <label for="{{ $name }}">{{ $label }}</label>
+    @if($label != '')
+        <label for="{{ $name }}" class="form-check-label">{{ $label }}</label>
+    @endif
 </div>
