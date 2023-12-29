070edb6fe48c4a7e1f9e92c498a9f83f87236b18
silviabielikova <XCdSmKj3hXE6Gpn>
2023-12-29 16:51:39 +0100

    fixed content section and input layout

--- src/resources/views/components/content-section.blade.php
+++ src/resources/views/components/content-section.blade.php
@@ -1,12 +1,17 @@
 <fieldset {{ $attributes->merge(['class' => 'border-bottom pb-4 m-4']) }}>
-    <b>{{ $title }}</b>
-    <div class="mt-3 px-5">
+    @if($title != '')
+        <b>{{ $title }}</b>
+    @endif
+
+    <div class="mt-3 px-4">
         @if($description != "")
             <div class="mb-4">{{ $description }}</div>
         @endif
 
-        <div class="row ">
-            {{ $slot }}
+            <div class="container">
+            <div class="row ">
+                {{ $slot }}
+            </div>
         </div>
     </div>
 
--- src/resources/views/components/simple-input.blade.php
+++ src/resources/views/components/simple-input.blade.php
@@ -1,14 +1,16 @@
-<div class="{{ $getSize() }} form-group row">
-    <div class="col-sm-4 text-right">
-        @if($label != '')
-            <label for="{{ $name }}" class="col-form-label col-form-label-sm ">{{ $label }}</label>
-        @endif
-    </div>
+<div class="{{ $getSize() }} form-group">
+    <div class="row">
+        <div class="col-sm-4 text-right">
+            @if($label != '')
+                <label for="{{ $name }}" class="col-form-label col-form-label-sm ">{{ $label }}</label>
+            @endif
+        </div>
 
-    <div class="col-sm-8">
-        <input class="form-control form-control-sm" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ $value == '' ? old($name) : $value }}" {{ $isReadOnly() ? 'readonly' : '' }} />
-        @error($name)
-        <p>{{$message}}</p>
-        @enderror
+        <div class="col-sm-8">
+            <input class="form-control form-control-sm" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ $value == '' ? old($name) : $value }}" {{ $isReadOnly() ? 'readonly' : '' }} />
+            @error($name)
+            <p>{{$message}}</p>
+            @enderror
+        </div>
     </div>
 </div>
