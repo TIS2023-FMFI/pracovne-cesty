413d5450043272073725a03882b9e68bc529c45e
silviabielikova <XCdSmKj3hXE6Gpn>
2024-01-03 17:32:56 +0100

    created spp manager form

--- src/resources/views/components/modals/spp-manager.blade.php
+++ src/resources/views/components/modals/spp-manager.blade.php
@@ -1,16 +1,28 @@
+@php
+    use App\Models\SppSymbol;
+    $spp_symbols = SppSymbol::all()->pluck('spp_symbol', 'id')->toArray();
+@endphp
+
 <x-modal title="ŠPP prvky" event="open-spp-manager" control="sppOpen">
-    <form action="spp/add">
-        @csrf
-        <x-content-section>
-            <x-simple-input name="email" label="E-mail:" size="long"></x-simple-input>
-        </x-content-section>
-        <x-button>Pridať používateľa</x-button>
-    </form>
     <form action="spp/deactivate">
         @csrf
-        <x-content-section>
-            <x-simple-input name="email" label="E-mail:" size="long"></x-simple-input>
+        <x-content-section title="Deaktivácia">
+            <x-dropdown-input name="spp_symbol" :values="$spp_symbols" label="ŠPP prvok:"></x-dropdown-input>
+            <x-button>Deaktivovať</x-button>
         </x-content-section>
-        <x-button>Pridať používateľa</x-button>
+
+    </form>
+    <form action="spp/store">
+        @csrf
+        <x-content-section title="Nový ŠPP prvok">
+            <x-simple-input name="fund" label="Fond:"></x-simple-input>
+            <x-simple-input name="spp_symbol" label="ŠPP prvok:"></x-simple-input>
+            <x-simple-input name="functional_region" label="Funkčná oblasť:"></x-simple-input>
+            <x-simple-input name="account" label="Účet:"></x-simple-input>
+            <x-simple-input name="financial_centre" label="Finančné stredisko:"></x-simple-input>
+            <x-simple-input name="grantee" label="Zodpovedný riešiteľ"></x-simple-input>
+            <x-button>Pridať</x-button>
+        </x-content-section>
+
     </form>
 </x-modal>
