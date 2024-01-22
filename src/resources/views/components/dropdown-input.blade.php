7029f5bf05f8bbd767e7a4eca589dec700afb206
silviabielikova <XCdSmKj3hXE6Gpn>
2024-01-22 02:14:18 +0100

    deleted unused code

--- src/resources/views/components/dropdown-input.blade.php
+++ src/resources/views/components/dropdown-input.blade.php
@@ -10,18 +10,3 @@
         @endforeach
     </select>
 </div>
-
-{{--<div class="form-group">--}}
-{{--    <div class="row">--}}
-{{--        <label for="{{ $name }}" class="col-sm-3 col-form-label col-form-label-sm text-right">{{ $label }}</label>--}}
-{{--        <div class="col-sm-9">--}}
-{{--            <select class="custom-select custom-select-sm" name="{{ $name }}" id="{{ $name }}" {{ $isDisabled() ? 'disabled' : '' }}>--}}
-{{--                @foreach ($values as $id => $name)--}}
-{{--                    <option value="{{ $id }}" {{ $isSelected($id) ? 'selected' : '' }}>--}}
-{{--                        {{ $name }}--}}
-{{--                    </option>--}}
-{{--                @endforeach--}}
-{{--            </select>--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
--- src/resources/views/components/simple-input.blade.php
+++ src/resources/views/components/simple-input.blade.php
@@ -11,21 +11,3 @@
 
 </div>
 
-
-{{--<div class="form-group">--}}
-{{--    <div class="row">--}}
-{{--        <div class="col-sm-3 text-right">--}}
-{{--            @if($label != '')--}}
-{{--                <label for="{{ $name }}" class="col-form-label col-form-label-sm ">{{ $label }}</label>--}}
-{{--            @endif--}}
-{{--        </div>--}}
-
-{{--        <div class="col-sm-9">--}}
-{{--            <input class="form-control form-control-sm" type="{{ $type }}" id="{{ $name }}" name="{{ $name }}" value="{{ $value == '' ? old($name) : $value }}" {{ $isReadOnly() ? 'readonly' : '' }} />--}}
-{{--            @error($name)--}}
-{{--            <p>{{$message}}</p>--}}
-{{--            @enderror--}}
-{{--        </div>--}}
-{{--    </div>--}}
-{{--</div>--}}
-
