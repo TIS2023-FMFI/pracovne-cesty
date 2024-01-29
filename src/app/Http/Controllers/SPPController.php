98dd72dac165f01ac381607a57136b5976a48390
silviabielikova <sisa@bielik.sk>
2024-01-29 23:55:00 +0100

    implemented SPP controller methods

--- src/app/Http/Controllers/SPPController.php
+++ src/app/Http/Controllers/SPPController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\SppSymbol;
+use App\Enums\SppStatus;
 use Illuminate\Http\Request;
 
 class SPPController extends Controller
@@ -12,15 +14,27 @@ class SPPController extends Controller
      * Sending all active spp symbols
      */
     public function manage() {
-        return view();
+        return view('spp.manage', ['spp_symbols' => SppSymbol::where('status', SppStatus::ACTIVE)->pluck('spp_symbol', 'id')]);
     }
 
     /**
      * Same as store() in BusinessTrip
-     *Validating data
+     * Validating data
      * Saving new spp symbol in DB
      */
     public function store(Request $request) {
+        $validatedData = $request->validate([
+            'fund' => 'required|string',
+            'spp_symbol' => 'required|string|unique:spp_symbols,spp_symbol',
+            'functional_region' => 'required|string',
+            'account' => 'required|string',
+            'financial_centre' => 'required|string',
+            'grantee' => 'required|string|max:200',
+            ]);
+
+        SppSymbol::create($validatedData);
+
+        return redirect()->route('spp.manage')->with('message', 'ŠPP prvok bol pridaný.');
 
     }
 
@@ -28,7 +42,11 @@ class SPPController extends Controller
      * Input parameter of type Request and the spp symbol
      * Updating the spp symbols state to deactivated
      */
-    public function deactivate(Request $request, Spp $spp) {
+//    public function deactivate(Request $request, SppSymbol $spp) {
+    public function deactivate(Request $request) {
+        $validatedData = $request->validate(['spp' => 'exists:spp_symbols,id']);
+        SppSymbol::find($validatedData['spp'])->update(['status' => SppStatus::DEACTIVATED]);
 
+        return redirect()->route('spp.manage')->with('message', 'ŠPP prvok bol deaktivovaný.');
     }
 }
