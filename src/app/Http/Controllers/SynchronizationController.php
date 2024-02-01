771f545eb24a1af179a289bfbd608de06483e18e
mcibula <mirkocibula@gmail.com>
2024-02-01 02:12:28 +0100

    Assign role to users imported from Pritomnost

--- src/app/Http/Controllers/SynchronizationController.php
+++ src/app/Http/Controllers/SynchronizationController.php
@@ -63,6 +63,8 @@ class SynchronizationController extends Controller
 
                 // Add type of user
                 $newUser->user_type = UserType::EMPLOYEE;
+                $newUser->assignRole('traveller');
+
                 $newUser->save();
             }
         }
