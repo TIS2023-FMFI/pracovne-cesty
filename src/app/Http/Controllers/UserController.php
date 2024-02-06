d12284540d60baeb0762ca3ff2b0853e51c7812d
mcibula <mirkocibula@gmail.com>
2024-02-06 15:39:44 +0100

    Fix invalid dashboard redirect

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -105,7 +105,7 @@ class UserController extends Controller
             }
             if (Auth::attempt($credentials)) {
                 $request->session()->regenerate();
-                return redirect()->intended('dashboard');
+                return redirect()->intended('homepage');
             }
         }
 
