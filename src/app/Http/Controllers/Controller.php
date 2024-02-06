4109031429ac45344236e32361f6589833116538
ihorbaltsat <baltsat1@uniba.sk>
2024-02-06 08:21:49 +0100

    add key replace function

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -9,4 +9,17 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected static function array_key_replace($item, $replace_with, array $array): array
+    {
+        $updated_array = [];
+        foreach ($array as $key => $value) {
+            if (!is_array($value) && $key == $item) {
+                $updated_array = array_merge($updated_array, [$replace_with => $value]);
+                continue;
+            }
+            $updated_array = array_merge($updated_array, [$key => $value]);
+        }
+        return $updated_array;
+    }
 }
