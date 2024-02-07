cc965d3a817bd275c4b32cdb6a9f644ddbf2af95
samoslavik45 <samuelslavik1010@gmail.com>
2024-02-07 23:53:30 +0100

    Pridanie error messagess

--- src/app/Http/Requests/UserRegistrationRequest.php
+++ src/app/Http/Requests/UserRegistrationRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UserRegistrationRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class UserRegistrationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return Auth::check();
     }
 
     /**
