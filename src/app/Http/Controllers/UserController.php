02f153249b6322bdb79abeed3b4aa0a106aeeeba
samoslavik45 <samuelslavik1010@gmail.com>
2024-02-08 22:02:23 +0100

    Updatnuta verzia

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\View\View;
+use App\Http\Requests\UserRegistrationRequest;
 
 
 class UserController extends Controller
--- src/app/Http/Requests/UserRegistrationRequest.php
+++ src/app/Http/Requests/UserRegistrationRequest.php
@@ -12,7 +12,7 @@ class UserRegistrationRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return Auth::check();
+        return true;
     }
 
     /**
