12d844b65712f7a1fc609a81cf69816c43ea6ae3
silviabielikova <sisa@bielik.sk>
2024-02-10 02:32:38 +0100

    added some styling

--- src/resources/views/components/content-box.blade.php
+++ src/resources/views/components/content-box.blade.php
@@ -2,7 +2,7 @@
 
 <div class="my-2">
     <div {{ $attributes->merge(['class' => 'p-3 bg-dark text-uppercase text-white font-weight-bold']) }}>{{ $title }}</div>
-    <div class="bg-white border border-top-0 p-4">
+    <div class="bg-white border border-top-0 p-3">
         {{ $slot }}
     </div>
 </div>
