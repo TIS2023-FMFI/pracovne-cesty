f9340753d6257bb9281c584b62a073b055de2d6a
silviabielikova <sisa@bielik.sk>
2024-02-12 00:30:35 +0100

    used x-cloak on flash message

--- src/resources/views/components/flash-message.blade.php
+++ src/resources/views/components/flash-message.blade.php
@@ -1,6 +1,11 @@
 @if(session()->has('message'))
-    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show"
-         class="fixed-top bg-secondary text-white px-3 py-2">
+    <div
+        x-data="{ show: true }"
+        x-init="setTimeout(() => show = false, 3000)"
+        x-show="show"
+        x-cloak
+        class="fixed-top bg-secondary text-white px-3 py-2">
+
         <p>
             {{ session('message') }}
         </p>
