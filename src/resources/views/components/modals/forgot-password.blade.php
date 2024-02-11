34723b2220add2f239f54935fd26539e198df54e
silviabielikova <sisa@bielik.sk>
2024-02-12 00:33:52 +0100

    updated 'add user' modal and created 'forgot password' modal

---
+++ src/resources/views/components/modals/forgot-password.blade.php
@@ -0,0 +1,15 @@
+<x-modal title="Zabudnuté heslo" name="forgot-password">
+    <form action="/forgot-password" method="POST">
+        @csrf
+        <x-content-section>
+            <x-slot:description>
+                <p>Zabudli ste heslo?</p>
+                <p>Zadajte svoj email, bude vám zaslaná adresoa, kde si môžete nastaviť nové.</p>
+            </x-slot:description>
+            <x-simple-input name="email" label="Emailová adresa"></x-simple-input>
+        </x-content-section>
+        <div class="d-flex justify-content-end">
+            <x-button>Odoslať</x-button>
+        </div>
+    </form>
+</x-modal>
