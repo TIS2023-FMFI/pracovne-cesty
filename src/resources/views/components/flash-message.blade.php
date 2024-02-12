ae23ceb174f12d488fb5cd022d9e7a3714485350
silviabielikova <sisa@bielik.sk>
2024-02-12 22:10:33 +0100

    removed x-cloak from flash message

--- src/resources/views/components/flash-message.blade.php
+++ src/resources/views/components/flash-message.blade.php
@@ -3,11 +3,21 @@
         x-data="{ show: true }"
         x-init="setTimeout(() => show = false, 3000)"
         x-show="show"
-        x-cloak
         class="fixed-top bg-secondary text-white px-3 py-2">
 
-        <p>
-            {{ session('message') }}
-        </p>
+        <p>{{ session('message') }}</p>
+    </div>
+@endif
+
+@if(session()->has('warning'))
+    <div
+        x-data="{ show: true }"
+        x-show="show"
+        class="fixed-top bg-secondary text-white px-3 py-2">
+
+        <div class="flex justify-between">
+            <p>{{ session('warning') }}</p>
+            <button @click="show = false">Rozumiem</button>
+        </div>
     </div>
 @endif
