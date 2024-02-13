5c546e9d407ab58cb8a99f319f98439d085a0155
samoslavik45 <samuelslavik1010@gmail.com>
2024-02-13 02:38:41 +0100

    Added error-messages

--- src/app/Http/Controllers/BusinessTripController.php
+++ src/app/Http/Controllers/BusinessTripController.php
@@ -698,7 +698,7 @@ class BusinessTripController extends Controller
             'personal_id' => 'required|string|max:10',
             'department' => 'required|string|max:10',
             'address' => $rule . '|string|max:200',
-        ]);
+        ], self::customMessages(), self::customAttributes());
         return $validatedUserData;
     }
 
@@ -740,7 +740,7 @@ class BusinessTripController extends Controller
 //            ]);
 //        }
         //Validate trip data
-        return $request->validate($rules);
+        return $request->validate($rules, self::customMessages(), self::customAttributes());
     }
 
     /**
@@ -755,7 +755,7 @@ class BusinessTripController extends Controller
             'place' => 'required|string|max:200',
             'trip_purpose_id' => 'required|integer|min:0',
             'purpose_details' => 'nullable|string|max:50'
-        ]);
+        ], self::customMessages(), self::customAttributes());
         return $validatedData;
     }
 
@@ -769,7 +769,7 @@ class BusinessTripController extends Controller
             $validatedReimbursementData = $request->validate([
                 'reimbursement_spp_symbol_id' => 'required|exists:spp_symbols,id',
                 'reimbursement_date' => 'required|date',
-            ]);
+            ], self::customMessages(), self::customAttributes());
 
             return array(true, self::array_key_replace(
                 'reimbursement_spp_symbol_id',
@@ -794,7 +794,7 @@ class BusinessTripController extends Controller
                 'organiser_address' => 'required|string|max:200',
                 'organiser_iban' => 'required|string|max:34',
                 'amount' => 'required|string|max:20',
-            ]);
+            ], self::customMessages(), self::customAttributes());
 
             return array(true, self::array_key_replace(
                 'organiser_iban',
@@ -824,7 +824,7 @@ class BusinessTripController extends Controller
             $validatedExpenseData = $request->validate([
                 $expenseName . '_expense_eur' => 'nullable|string|max:20',
                 $expenseName . '_expense_not_reimburse' => 'nullable'
-            ]);
+            ], self::customMessages(), self::customAttributes());
             if ($trip->type === TripType::FOREIGN) {
                 $validatedExpenseData = array_merge($validatedExpenseData, $request->validate([
                     $expenseName . '_expense_foreign' => 'nullable|string:max:20']));
@@ -836,7 +836,7 @@ class BusinessTripController extends Controller
 
         $validatedExpensesData = array_merge($validatedExpensesData, $request->validate([
             'expense_estimation' => 'nullable|string|max:20'
-        ]));
+        ], self::customMessages(), self::customAttributes()));
 
         $validatedExpensesData['no_meals_reimbursed'] = $request->has('no_meals_reimbursed');
 //        dd($validatedExpensesData);
@@ -895,7 +895,7 @@ class BusinessTripController extends Controller
             if ($request->has('contribution_' . $id)) {
                 $validatedContributionData = $request->validate([
                     'contribution_' . $id . '_detail' => 'nullable|string|max:200',
-                ]);
+                ], self::customMessages(), self::customAttributes());
                 $updContributionData = self::array_key_replace(
                     'contribution_' . $id . '_detail',
                     'detail',
@@ -942,4 +942,80 @@ class BusinessTripController extends Controller
             TripContribution::create($contribution);
         }
     }
+    /**
+     * Get custom messages for validator errors.
+     *
+     * @return array
+     */
+    public static function customMessages()
+    {
+        return [
+            'required' => 'Pole :attribute je povinné.',
+            'string' => 'Pole :attribute musí byť reťazec.',
+            'max' => [
+                'string' => 'Pole :attribute môže mať maximálne :max znakov.',
+                'numeric' => 'Pole :attribute nesmie byť väčšie ako :max.',
+            ],
+            'date' => 'Pole :attribute musí byť platný dátum.',
+            'after' => 'Pole :attribute musí byť dátum po :date.',
+            'exists' => 'Vybrané pole :attribute je neplatné.',
+            'unique' => 'Pole :attribute už bolo zaregistrované.',
+            'email' => 'Pole :attribute musí byť platná e-mailová adresa.',
+            'confirmed' => 'Potvrdenie :attribute sa nezhoduje.',
+            'integer' => 'Pole :attribute musí byť celé číslo.',
+            'min' => [
+                'numeric' => 'Pole :attribute musí byť aspoň :min.',
+                'string' => 'Pole :attribute musí obsahovať aspoň :min znakov.',
+            ],
+            'nullable' => 'Pole :attribute môže byť prázdne.',
+        ];
+    }
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array
+     */
+    public static function customAttributes()
+    {
+        return [
+            'first_name' => 'meno',
+            'last_name' => 'priezvisko',
+            'academic_degrees' => 'akademický titul',
+            'department' => 'pracovisko',
+            'address' => 'adresa',
+            'country_id' => 'krajina',
+            'transport_id' => 'doprava',
+            'spp_symbol_id' => 'symbol SPP',
+            'place' => 'miesto',
+            'place_start' => 'miesto začiatku',
+            'place_end' => 'miesto konca',
+            'datetime_start' => 'dátum začiatku',
+            'datetime_end' => 'dátum konca',
+            'trip_purpose_id' => 'účel cesty',
+            'purpose_details' => 'detaily účelu',
+            'iban' => 'IBAN',
+            'reimbursement_spp_symbol_id' => 'symbol SPP pre náhradu',
+            'reimbursement_date' => 'dátum náhrady',
+            'organiser_name' => 'názov organizácie',
+            'ico' => 'IČO',
+            'organiser_address' => 'adresa organizátora',
+            'organiser_iban' => 'IBAN organizátora',
+            'amount' => 'suma',
+            'contribution_{id}' => 'príspevok',
+            'reimbursement' => 'náhrada',
+            'conference_fee' => 'konferenčný poplatok',
+            'travelling_expense_eur' => 'cestovné náklady v EUR',
+            'accommodation_expense_eur' => 'náklady na ubytovanie v EUR',
+            'allowance_expense_eur' => 'príspevok v EUR',
+            'advance_expense_eur' => 'záloha v EUR',
+            'other_expense_eur' => 'ostatné náklady v EUR',
+            'travelling_expense_foreign' => 'cestovné náklady v cudzej mene',
+            'accommodation_expense_foreign' => 'náklady na ubytovanie v cudzej mene',
+            'allowance_expense_foreign' => 'príspevok v cudzej mene',
+            'advance_expense_foreign' => 'záloha v cudzej mene',
+            'other_expense_foreign' => 'ostatné náklady v cudzej mene',
+            'reimburse' => 'náhrada',
+        ];
+    }
+
 }
