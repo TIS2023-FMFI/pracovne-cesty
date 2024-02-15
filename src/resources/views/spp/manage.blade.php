75e9885efc312c6bb632cf7cbe86afb51f975b89
silviabielikova <sisa@bielik.sk>
2024-02-15 16:17:20 +0100

    fixed spp frontend and controller

--- src/resources/views/spp/manage.blade.php
+++ src/resources/views/spp/manage.blade.php
@@ -6,10 +6,10 @@
                 @method('PUT')
                 <x-content-section title="Deaktivácia">
                     <div class="form-row">
-                        <div class="col-md-6 col-sm-12">
+                        <div class="col-md-6 col-12">
                             <x-dropdown-input name="spp" :values="$spp_symbols" label="ŠPP prvok:"></x-dropdown-input>
                         </div>
-                        <div class="col-md-6 col-sm-12">
+                        <div class="col-md-6 col-12">
                             <div class="d-flex justify-content-md-start justify-content-end h-100">
                                 <div class="form-group align-self-end">
                                     <x-button color="danger">Deaktivovať</x-button>
@@ -26,27 +26,24 @@
             @csrf
             <x-content-section title="Nový ŠPP prvok">
                 <div class="form-row">
-                    <div class="col-md-6 col-sm-12">
+                    <div class="col-md-6 col-12">
                         <x-simple-input name="fund" label="Fond:"></x-simple-input>
                     </div>
-                    <div class="col-md-6 col-sm-12">
+                    <div class="col-md-6 col-12">
                         <x-simple-input name="spp_symbol" label="ŠPP prvok:"></x-simple-input>
                     </div>
 
                 </div>
                 <div class="form-row">
-                    <div class="col-md-6 col-sm-12">
+                    <div class="col-md-6 col-12">
                         <x-simple-input name="functional_region" label="Funkčná oblasť:"></x-simple-input>
                     </div>
-                    <div class="col-md-6 col-sm-12">
-                        <x-simple-input name="account" label="Účet:"></x-simple-input>
+                    <div class="col-md-6 col-12">
+                        <x-simple-input name="financial_centre" label="Finančné stredisko:"></x-simple-input>
                     </div>
                 </div>
                 <div class="form-row">
-                    <div class="col-md-6 col-sm-12">
-                        <x-simple-input name="financial_centre" label="Finančné stredisko:"></x-simple-input>
-                    </div>
-                    <div class="col-md-6 col-sm-12">
+                    <div class="col-md-6 col-12">
                         <x-simple-input name="grantee" label="Zodpovedný riešiteľ"></x-simple-input>
                     </div>
                 </div>
