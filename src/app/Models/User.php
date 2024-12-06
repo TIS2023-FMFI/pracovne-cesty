864e2e3d8299e91ae938397616ce249b9bdf0c23
juraj326 <cernozem-serafin-00@icloud.com>
2024-12-06 10:34:06 +0100

    refactor: added return type to getSortedByLastName method

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -74,7 +74,7 @@ class User extends Authenticatable
         return $this->first_name . ' ' . $this->last_name;
     }
 
-    public static function getSortedByLastName() {
+    public static function getSortedByLastName() : Collection {
         return self::orderBy('last_name', 'asc')->get();
     }
 }
