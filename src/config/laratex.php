e5e4aa66a60679e7d0fd74d493976ce8adc14ce6
mcibula <mirkocibula@gmail.com>
2024-12-28 02:53:01 +0100

    Fix LaraTeX config

--- src/config/laratex.php
+++ src/config/laratex.php
@@ -2,13 +2,13 @@
 
 return [
 	// bin path to your pdflatex installation | use 'which pdflatex' on a linux system to find out which is the path to your pdflatex installation
-	'binPath' => 'pdflatex',
+	'binPath' => '/usr/bin/pdflatex',
 
 	// bin path to your bibtex installation | use 'which bibtex' on a linux system to find out which is the path to your bibtex installation
-	'bibTexPath' => 'bibtex',
+	'bibTexPath' => '/usr/bin/bibtex',
 
 	// Folder in your storage folder where you would like to store the temp files created by LaraTeX
-	'tempPath' => 'app/',
+	'tempPath' => 'app/pdf_exports',
 
 	// boolean to define if log, aux and tex files should be deleted after generating PDF
 	'teardown' => true,
